e6dd4899e417b8b20508c9b1628ae302ce9f24fe
JettMingin <155850120+JettMingin@users.noreply.github.com>
2024-01-15 17:59:14 -0500

    Update linux_index.pl

--- linux_index.pl
+++ linux_index.pl
@@ -2,20 +2,21 @@
 
 use v5.28;
 use strict;
-use warnings;
 
 #using bash alias "alias index='/[YOUR FILE PATH]/linux_index.pl'"
 
+my $indexDOC = '/[YOUR FILE PATH]/indexDOC.txt'; #Declare your file path to the indexDOC here
+
 sub read_index{
-	open INDEX, '/[YOUR FILE PATH]/indexDOC.txt';
+	open INDEX, "$_[0]";
 	chomp (my @indexDOC = <INDEX>);
+	close (INDEX);
 	
 	my %index = @indexDOC;
-
 	my @terms = sort (keys %index);
 
-	say "*** THE INDEX ***";
-	say "*reading from indexDOC.txt*\n";
+	printf "%42s", "*** THE INDEX ***\n";
+	say "*reading from $_[0]*\n";
 	my $linecount = 1;
 		
 	foreach my $i (0..$#terms){
@@ -26,41 +27,47 @@ sub read_index{
 }
 	
 sub search_index{
-	open INDEX, '/[YOUR FILE PATH]/indexDOC.txt';
+	open INDEX, "$_[0]";
 	chomp (my @indexDOC = <INDEX>);
+	close (INDEX);
 	
 	my %index = @indexDOC;
-
 	my @terms = sort (keys %index);
+	my $termchecker = -1;
 		
-	say "***INDEX SEARCH***";
-	say "Enter your desired search term:";
+	printf "%24s", "***INDEX SEARCH***\n";
+	print "Enter your desired search term: ";
 	chomp (my $searchterm = <STDIN>);
 	print "\n";
 		
 	foreach my $i (0..$#terms){
 		chomp $terms[$i];
-		if ($terms[$i] =~ /\A$searchterm/){
+		if ($terms[$i] =~ /\A$searchterm/i){
 			print "$terms[$i] - $index{$terms[$i]}\n\n";
-		}
+		}else{
+			$termchecker ++;
+			}
+	}
+	if ($termchecker == $#terms){
+		say "Sorry, I couldnt find that term in the index.";
 	}
 }
 	
-	
 sub update_index{
-	my $index = '/[YOUR FILE PATH]/indexDOC.txt';
-	open INDEX, ">> $index";
+	open INDEX, ">> $_[0]";
 	print "Enter your term\n";
 	my $a = <STDIN>;
 	print INDEX $a;
 	print "Enter your description\n";
 	my $b = <STDIN>;
 	print INDEX $b;
+	close INDEX;
+	chomp $a;
 	print "$a Sucessfully added to the doc\n";
 }
 	
 sub delete_index{
-	open INDEX, '/[YOUR FILE PATH]/indexDOC.txt';
+	open INDEX, "$_[0]";
 	chomp (my @indexDOC = <INDEX>);
 	close INDEX;
 		
@@ -84,10 +91,10 @@ sub delete_index{
 			print "Are you sure you wish to delete this term? (y/n): ";
 			my $delete_check = <STDIN>;
 			if ($delete_check =~ /\Ay\Z/){
-				open (INDEX, "</[YOUR FILE PATH]/indexDOC.txt");
+				open (INDEX, "<$_[0]");
 				my @LINES = <INDEX>;
 				close (INDEX);
-				open (INDEX, ">/[YOUR FILE PATH]/indexDOC.txt");
+				open (INDEX, ">$_[0]");
 				foreach my $LINE (@LINES){
 					print INDEX $LINE unless ($LINE =~ m/\A($TERM)|($DESCRIPTION)\Z/);
 				}
@@ -108,18 +115,17 @@ sub delete_index{
 }
 
 
-
-	if($ARGV[0] =~ /r/i){
-		read_index;
-	}elsif($ARGV[0] =~ /u/i){
-		update_index;
-	}elsif($ARGV[0] =~ /s/i){
-		search_index;
-	}elsif($ARGV[0] =~ /x/i){
-		delete_index;
-	}else{
-		say "Enter 'index r' to see index";
-		say "Enter 'index u' to add a term and description";
-		say "Enter 'index s' to search for terms";
-		say "Enter 'index x' to delete a term";
-	}
+if($ARGV[0] =~ /\Ar\Z/i){
+	read_index ($indexDOC);
+}elsif($ARGV[0] =~ /\Au\Z/i){
+	update_index ($indexDOC);
+}elsif($ARGV[0] =~ /\As\Z/i){
+	search_index ($indexDOC);
+}elsif($ARGV[0] =~ /\Ax\Z/i){
+	delete_index ($indexDOC);
+}else{
+	say "Enter 'index r' to see the entire index";
+	say "Enter 'index u' to add a term and description";
+	say "Enter 'index s' to search for terms";
+	say "Enter 'index x' to delete a term";
+}
